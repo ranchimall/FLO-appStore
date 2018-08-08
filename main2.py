93cf6c9a833f65c8b9bbfc771769915280256cd2
Kaushal Kumar Agarwal <kaushalag29@gmail.com>
2018-08-08 14:58:57 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from tkinter import *
+import webbrowser
 from tkinter import messagebox
 import subprocess
 
@@ -7,6 +8,9 @@ import subprocess
 class FLOappStore:
 	def __init__(self, root):
 		self.root = root
+
+	def medici(self):
+		webbrowser.open("https://www.mediciventures.com/",new=1)
 
 	def start(self):
 		self.MainFrame = Frame(self.root, height=1000,width=500)
@@ -17,7 +21,7 @@ class FLOappStore:
 		button1 = Button(self.MainFrame,image = self.img1,width="187",height="186",command=lambda:self.execute('apps/FLO-Shared-Secret/FLO_Secret'))
 		button1.grid(row=2,column=1)
 		self.img2 = PhotoImage(file="Logos/Medici.png")
-		button2 = Button(self.MainFrame, image=self.img2, width="187", height="186",command=lambda: self.execute('apps/FLO-Shared-Secret/FLO_Secret'))
+		button2 = Button(self.MainFrame, image=self.img2, width="187", height="186",command=self.medici)
 		button2.grid(row=2, column=2)
 
 	def execute(self,f):
