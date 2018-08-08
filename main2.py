6dacce4b9a145f217b962d910f77a296d26b6cf4
Kaushal Kumar Agarwal <kaushalag29@gmail.com>
2018-08-08 14:30:08 +0530

    Alignment of logos fix

---
+++ main2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from tkinter import *
+from tkinter import messagebox
+import subprocess
+
+
+class FLOappStore:
+	def __init__(self, root):
+		self.root = root
+
+	def start(self):
+		self.MainFrame = Frame(self.root, height=1000,width=500)
+		self.MainFrame.pack()
+		WelcomeLabel = Label(self.MainFrame,text="FLO AppStore",font=("Arial", 20))
+		WelcomeLabel.grid(column = 1, columnspan =4)
+		self.img1 = PhotoImage(file="Logos/SharedSecret.png")
+		button1 = Button(self.MainFrame,image = self.img1,width="187",height="186",command=lambda:self.execute('apps/FLO-Shared-Secret/FLO_Secret'))
+		button1.grid(row=2,column=1)
+		self.img2 = PhotoImage(file="Logos/Medici.png")
+		button2 = Button(self.MainFrame, image=self.img2, width="187", height="186",command=lambda: self.execute('apps/FLO-Shared-Secret/FLO_Secret'))
+		button2.grid(row=2, column=2)
+
+	def execute(self,f):
+		subprocess.run([f])
+root = Tk()
+root.title("FLOappStore")
+gui = FLOappStore(root)
+gui.start()
+root.mainloop()
